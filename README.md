876ab6f5b950b24755702e954c52ba2c6e631b60
Hansimov <591172499@qq.com>
2024-10-07 08:21:30 +0800

    :zap: [Enhance] prettify console output in main, and add go mod init command in README

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # ipv6-proxy-go
 A service in go to proxy network requests with ipv6 addrs
+
+## Init
+
+```sh
+go mod init ipv6-proxy-go
+```
--- main.go
+++ main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"ipv6-proxy-go/ipv6"
 )
 
 func main() {
-	ipv6.GetIPv6Prefix()
+	ipv6_info := ipv6.GetIPv6Info(ipv6.Args{Verbose: false})
+	pretty_json, _ := json.MarshalIndent(ipv6_info, "", "  ")
+	fmt.Println(string(pretty_json))
 }
 
 // go run main.go
